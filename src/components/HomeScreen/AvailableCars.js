17e5d891b18afd5aa50e1c73222c1b364fef89de
Yosra Emad <yosraemad14@gmail.com>
2022-01-09 21:26:43 +0200

    Added car filtering

--- src/components/HomeScreen/AvailableCars.js
+++ src/components/HomeScreen/AvailableCars.js
@@ -5,36 +5,7 @@ import CarItem from "../UI/caritem";
 import styles from "../../styles/cars.module.css";
 
 const AvailableCars = (props) => {
-  const [cars, setCars] = useState([]);
-  const { isLoading, error, sendRequest } = useHttp();
-
-  useEffect(async () => {
-    const setCarData = (responseData) => {
-      if (responseData) {
-        let carsArray = [];
-        for (let i = 0; i < responseData.length; i++) {
-          carsArray.push(responseData[i]);
-        }
-        setCars(carsArray);
-      }
-    };
-    const getCars = async () => {
-      const response = await sendRequest(
-        {
-          url: "http://localhost:5000/api/v1/office-cars/details",
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + props.token,
-          },
-        },
-        setCarData.bind(null)
-      );
-    };
-    await getCars();
-  }, []);
-
-  const carItems = cars.map((car) => {
+  const carItems = props.cars.map((car) => {
     return <CarItem key={car.car_id} carDescription={car.Car.CarDescription} />;
   });
 
--- src/pages/home-screen.js
+++ src/pages/home-screen.js
@@ -1,15 +1,77 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import CarItem from "../components/UI/caritem";
-import { useContext } from "react";
 import { Link } from "react-router-dom";
 import AccountContext from "../models/account";
 import AvailableCars from "../components/HomeScreen/AvailableCars";
+import useHttp from "../hooks/use-http";
+import Filters from "../components/HomeScreen/Filters";
 const HomeScreen = () => {
   const accCtx = useContext(AccountContext);
+  const [cars, setCars] = useState([]);
+  const { isLoading, error, sendRequest } = useHttp();
+  const [offices, setOffices] = useState([]);
+
+  useEffect(async () => {
+    const setCarData = (responseData) => {
+      if (responseData) {
+        let carsArray = [];
+        for (let i = 0; i < responseData.length; i++) {
+          carsArray.push(responseData[i]);
+        }
+        setCars(carsArray);
+        console.log(carsArray);
+      }
+    };
+    const getCars = async () => {
+      const response = await sendRequest(
+        {
+          url: "http://localhost:5000/api/v1/office-cars/details",
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + accCtx.token,
+          },
+        },
+        setCarData.bind(null)
+      );
+    };
+
+    const setOfficeData = (responseData) => {
+      if (responseData) {
+        let officesArray = [];
+        for (let i = 0; i < responseData.length; i++) {
+          officesArray.push(responseData[i]);
+        }
+        setOffices(officesArray);
+      }
+    };
+
+    const getOffices = async () => {
+      const response = await sendRequest(
+        {
+          url: "http://localhost:5000/api/v1/offices",
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + accCtx.token,
+          },
+        },
+        setOfficeData.bind(null)
+      );
+    };
+    await getCars();
+    await getOffices();
+  }, []);
 
   return accCtx.token ? (
     <>
-      <AvailableCars token={accCtx.token} />
+      <Filters
+        offices={offices}
+        currentOffice={cars[0] && cars[0].office_id}
+        setCars={setCars}
+        token={accCtx.token}
+      />
+      <AvailableCars cars={cars} />
     </>
   ) : (
     <h1>
